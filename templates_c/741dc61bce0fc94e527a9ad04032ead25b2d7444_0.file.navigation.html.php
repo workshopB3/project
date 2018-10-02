21c183dfe52ba871cd181374bd534459e9318776
Laplo <ronanlaplaud4@gmail.com>
2018-10-02 15:48:30 +0200

    some css modif

--- templates_c/741dc61bce0fc94e527a9ad04032ead25b2d7444_0.file.navigation.html.php
+++ templates_c/741dc61bce0fc94e527a9ad04032ead25b2d7444_0.file.navigation.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.33, created on 2018-10-02 15:10:22
+/* Smarty version 3.1.33, created on 2018-10-02 15:47:20
   from '/Users/ronanlaplaud/Documents/project/html/navigation.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.33',
-  'unifunc' => 'content_5bb36e3ea7c995_82620576',
+  'unifunc' => 'content_5bb376e84223c8_91618934',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '741dc61bce0fc94e527a9ad04032ead25b2d7444' => 
     array (
       0 => '/Users/ronanlaplaud/Documents/project/html/navigation.html',
-      1 => 1538485821,
+      1 => 1538488030,
       2 => 'file',
     ),
   ),
@@ -20,10 +20,10 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5bb36e3ea7c995_82620576 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5bb376e84223c8_91618934 (Smarty_Internal_Template $_smarty_tpl) {
 ?><link rel="stylesheet" href="web/css/navigation.css">
-<div id="img-content">
-  <img id="navImg" width="800" height="800" style="position: absolute;z-index: 1;width:100%; height:100%;" src="<?php echo $_smarty_tpl->tpl_vars['img']->value[0]['url'];?>
+<div class="img-content" id="img-content">
+  <img class="navImg" id="navImg" width="800" height="800" style="position: absolute;z-index: 1;width:100%; height:100%;" src="<?php echo $_smarty_tpl->tpl_vars['img']->value[0]['url'];?>
 ">
     <img id="arrowLeft" class="fleche" style="left:5%;top:45%" src="./assets/imgs/fleche_gauche.png" onclick="changeSrcImg('left');"></img>
     <img id="arrowRight" class="fleche" style="right:5%;top:45%" src="./assets/imgs/fleche_droite.png" onclick="changeSrcImg('right');"></img>
