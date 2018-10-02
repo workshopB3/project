947d6b9a984add9bdfa3b5818cdc13bae4216dfd
Laplo <ronanlaplaud4@gmail.com>
2018-10-02 16:40:35 +0200

    merge branches

--- templates_c/88653ef33fa9440eec90c3ebe9ae4e1071acbcd4_0.file.footer.html.php
+++ templates_c/88653ef33fa9440eec90c3ebe9ae4e1071acbcd4_0.file.footer.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.33, created on 2018-10-02 13:27:04
+/* Smarty version 3.1.33, created on 2018-10-02 16:40:18
   from '/Users/ronanlaplaud/Documents/project/html/footer.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.33',
-  'unifunc' => 'content_5bb35608ecc2b2_21512790',
+  'unifunc' => 'content_5bb38352f2e7b1_39076857',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '88653ef33fa9440eec90c3ebe9ae4e1071acbcd4' => 
     array (
       0 => '/Users/ronanlaplaud/Documents/project/html/footer.html',
-      1 => 1538479390,
+      1 => 1538491209,
       2 => 'file',
     ),
   ),
@@ -20,9 +20,12 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5bb35608ecc2b2_21512790 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5bb38352f2e7b1_39076857 (Smarty_Internal_Template $_smarty_tpl) {
 echo '<script'; ?>
- src="plugins/fullpage/dist/fullpage.min.js"><?php echo '</script'; ?>
+ src="plugins/fullpage/dist/fullpage.extensions.min.js"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"><?php echo '</script'; ?>
 >
 <?php echo '<script'; ?>
  src="web/js/js.js"><?php echo '</script'; ?>
---
+++ templates_c/efc97491526eb32dcb20448cc7b9cb960e752f5c_0.file.home.html.php
@@ -0,0 +1,29 @@
+<?php
+/* Smarty version 3.1.33, created on 2018-10-02 16:40:19
+  from '/Users/ronanlaplaud/Documents/project/html/onepage/home.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5bb38353008164_25143907',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'efc97491526eb32dcb20448cc7b9cb960e752f5c' => 
+    array (
+      0 => '/Users/ronanlaplaud/Documents/project/html/onepage/home.html',
+      1 => 1538491209,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5bb38353008164_25143907 (Smarty_Internal_Template $_smarty_tpl) {
+?><div class="section">
+    <img src="" width="100%;"/>
+      <h1>Le campus HEP de Nantes</h1>
+    </div>
+<?php }
+}
