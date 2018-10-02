ec97e661a0c15c3aaae4934d64c4c1c40ea1c821
Laplo <ronanlaplaud4@gmail.com>
2018-10-02 14:30:46 +0200

    add changes onclick change image

--- templates_c/741dc61bce0fc94e527a9ad04032ead25b2d7444_0.file.navigation.html.php
+++ templates_c/741dc61bce0fc94e527a9ad04032ead25b2d7444_0.file.navigation.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.33, created on 2018-10-02 14:13:22
+/* Smarty version 3.1.33, created on 2018-10-02 14:28:45
   from '/Users/ronanlaplaud/Documents/project/html/navigation.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.33',
-  'unifunc' => 'content_5bb360e2962c44_00797135',
+  'unifunc' => 'content_5bb3647da05951_24906941',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '741dc61bce0fc94e527a9ad04032ead25b2d7444' => 
     array (
       0 => '/Users/ronanlaplaud/Documents/project/html/navigation.html',
-      1 => 1538482401,
+      1 => 1538483308,
       2 => 'file',
     ),
   ),
@@ -20,31 +20,27 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5bb360e2962c44_00797135 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5bb3647da05951_24906941 (Smarty_Internal_Template $_smarty_tpl) {
 ?><div id="img-content">
   <img id="navImg" width="800" height="800" style="width:100%; height:100%;"src="<?php echo $_smarty_tpl->tpl_vars['img']->value[0]['url'];?>
-" onclick="changeSrcImg();"></img>
+" onclick="changeSrcImg('behind');"></img>
 </div>
 <?php echo '<script'; ?>
  language="javascript">
   var imgIndex = 0;
 
-  function changeSrcImg() {
+  function changeSrcImg(direction) {
     var encodedImg = <?php echo $_smarty_tpl->tpl_vars['encoded_img']->value;?>
 ;
     var currentObj = encodedImg[imgIndex];
-    console.log(currentObj);
     for (var i = 0; i<encodedImg.length; i++) {
       var obj = encodedImg[i];
-      if(obj.id == currentObj.forward) {
-        console.log(currentObj);
-        console.log(obj);
+      if(obj.id == currentObj[direction]) {
         currentObj = obj;
         imgIndex = i;
         break;
       }
     }
-
     document.getElementById("navImg").src = currentObj.url;
   }
 <?php echo '</script'; ?>
