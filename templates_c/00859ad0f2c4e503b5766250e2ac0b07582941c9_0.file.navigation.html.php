3e304240ccc4d479ba22ced2e2404a957c5fb379
Enki Michel <ikne@hotmail.fr>
2018-10-03 13:47:13 +0200

    amélioration de l'affichage

---
+++ templates_c/00859ad0f2c4e503b5766250e2ac0b07582941c9_0.file.navigation.html.php
@@ -0,0 +1,27 @@
+<?php
+/* Smarty version 3.1.33, created on 2018-10-02 14:22:04
+  from 'C:\wamp64\www\workshop\html\navigation.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5bb37f0c7c4993_23155027',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '00859ad0f2c4e503b5766250e2ac0b07582941c9' => 
+    array (
+      0 => 'C:\\wamp64\\www\\workshop\\html\\navigation.html',
+      1 => 1538478755,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5bb37f0c7c4993_23155027 (Smarty_Internal_Template $_smarty_tpl) {
+echo $_smarty_tpl->tpl_vars['img_url']->value;?>
+
+<?php }
+}
--- templates_c/ba1f4e8170908bb05e2f2d7a65d33c2424c3df44_0.file.specificities.html.php
+++ templates_c/ba1f4e8170908bb05e2f2d7a65d33c2424c3df44_0.file.specificities.html.php
@@ -1,4 +1,27 @@
-<div class="section" data-anchor="specificities" body style = "background-color:#4bbfc3">
+<?php
+/* Smarty version 3.1.33, created on 2018-10-03 11:34:45
+  from 'C:\wamp64\www\workshop\html\onepage\specificities.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5bb4a9558ee308_10916585',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'ba1f4e8170908bb05e2f2d7a65d33c2424c3df44' => 
+    array (
+      0 => 'C:\\wamp64\\www\\workshop\\html\\onepage\\specificities.html',
+      1 => 1538566483,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5bb4a9558ee308_10916585 (Smarty_Internal_Template $_smarty_tpl) {
+?><div class="section" data-anchor="specificities" body style = "background-color:#4bbfc3">
   <h1>Les richesses du campus</h1>
   </br>
   <p class="description">
@@ -33,3 +56,5 @@
   <p class="description">En plus des salles de cours, des espaces sont mis à disposition au sein des locaux du campus pour travailler ou se divertir.
                           On trouve notamment un espace pour jouer à la console ainsi qu’une table de ping pong. </p>
 </div>
+<?php }
+}
