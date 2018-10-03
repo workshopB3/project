00291c6b2e1f65e975a53080a0ad9a4d95e4edae
Laplo <ronanlaplaud4@gmail.com>
2018-10-03 09:35:32 +0200

    template

---
+++ templates_c/741dc61bce0fc94e527a9ad04032ead25b2d7444_0.file.navigation.html.php
@@ -0,0 +1,27 @@
+<?php
+/* Smarty version 3.1.33, created on 2018-10-03 09:34:38
+  from '/Users/ronanlaplaud/Documents/project/html/navigation.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5bb4710e551607_26998559',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '741dc61bce0fc94e527a9ad04032ead25b2d7444' => 
+    array (
+      0 => '/Users/ronanlaplaud/Documents/project/html/navigation.html',
+      1 => 1538552070,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5bb4710e551607_26998559 (Smarty_Internal_Template $_smarty_tpl) {
+echo $_smarty_tpl->tpl_vars['img_url']->value;?>
+
+<?php }
+}
