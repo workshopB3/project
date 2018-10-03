230c4c0b1e028dbc14f7151bd5fffffc16b01644
Houlgatte Louka <lhoulgattebustamante@gmail.com>
2018-10-03 20:04:23 +0200

    add pictures from whole building

--- templates_c/206ad1c293127caa3b8120198dbf95e23175c972_0.file.header.html.php
+++ templates_c/206ad1c293127caa3b8120198dbf95e23175c972_0.file.header.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.33, created on 2018-10-03 11:20:33
+/* Smarty version 3.1.33, created on 2018-10-03 19:18:56
   from 'C:\wamp\www\workshop\project\html\header.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.33',
-  'unifunc' => 'content_5bb489e1c6e943_06717320',
+  'unifunc' => 'content_5bb4fa00350942_69467342',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '206ad1c293127caa3b8120198dbf95e23175c972' => 
     array (
       0 => 'C:\\wamp\\www\\workshop\\project\\html\\header.html',
-      1 => 1538553774,
+      1 => 1538582877,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5bb489e1c6e943_06717320 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5bb4fa00350942_69467342 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -36,14 +36,14 @@ function content_5bb489e1c6e943_06717320 (Smarty_Internal_Template $_smarty_tpl)
 >
 </head>
 <body>
-	<?php if (!empty($_GET['page']) && $_GET['page'] == 'navigation') {?>
+	<?php if (!empty($_GET['page']) && ($_GET['page'] == 'navigation_choice' || $_GET['page'] == 'navigation')) {?>
 	<?php } else { ?>
 	<nav id="menu">
     <ul>
         <li data-menuanchor="Accueil"><a href="index.php">Accueil</a></li>
 				<li data-menuanchor="Ecoles"><a href="#ecoles">Nos écoles</a></li>
 				<li data-menuanchor="Ecoles"><a href="#">Les Richesses du campus</a></li>
-        <li data-menuanchor="Navigation"><a href="index.php?page=navigation">Navigation</a></li>
+        <li data-menuanchor="Navigation"><a href="index.php?page=navigation_choice">Navigation</a></li>
     </ul>
 	</nav>
 	<?php }
