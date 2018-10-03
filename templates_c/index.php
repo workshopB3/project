77f951949cd28c5c7fe47485b8af02e01c8e4218
Enki Michel <ikne@hotmail.fr>
2018-10-03 13:46:05 +0200

    amélioration de l'affichage

---
+++ templates_c/index.php
@@ -0,0 +1,13 @@
+<?php
+ob_start();
+session_start();
+include ('controller.php');
+$page = !empty($_GET["page"]) ? $_GET["page"] : 'home';
+$controller = new Controller();
+switch ($page){
+  case 'navigation': $controller->navigation();
+  break;
+  default: $controller->home();
+}
+
+?>
